e3e25b14f12e1fe869d755859352229ca3fa0b75
ArYa-KuWaR <aryakuwar@gmail.com>
2026-04-12 11:48:48 +0530

    fix typo in simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,3 +1,4 @@
+# fix typo
 #!/bin/bash
 # This script calculates simple interest given principal, annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
