53f2b202fafa0bfbca917348f006bdbf33fdb61f
Harry_lbi <harrylbi014@gmail.com>
2026-05-11 20:31:43 +0700

    contribusi untukk PR

---
+++ contirbusi.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script ini digunakan untuk menyapa kontributor baru.
+# Hanya untuk keperluan demo dalam kursus Git dan GitHub.
+
+echo "Selamat datang di proyek Introduction to Git and GitHub!"
+echo "Siapa nama Anda?"
+read name
+
+echo "Halo $name! Terima kasih telah berkontribusi di repositori ini."
+echo "Mari belajar Git dan GitHub bersama-sama!"
